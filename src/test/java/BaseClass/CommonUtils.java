60888ec5463c970b370d805bbf0254737593b332
swapnilGirishPawar <swapnil.girish@anywhere.co>
2024-05-31 13:35:15 +0530

    date and time checked

--- src/test/java/BaseClass/CommonUtils.java
+++ src/test/java/BaseClass/CommonUtils.java
@@ -1,7 +1,26 @@
 package BaseClass;
 
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
 public class CommonUtils {
     public static String pantherName(){
         return "Mogambo";
+    }
+    public static void divider(){
+        System.out.println("______________________________________________________________________");
+    }
+
+    @Test
+    public void testing(){
+        System.out.println(pantherName());
+        Date date = new Date();
+        divider();
+        System.out.println("Current Date :- "+LocalDate.now());
+        System.out.println("Current Time :- "+LocalTime.now());
+        divider();
     }
 }
--- src/test/java/Suite/HelloWorld.java
+++ src/test/java/Suite/HelloWorld.java
@@ -1,10 +1,15 @@
 package Suite;
 
+import org.testng.annotations.Test;
+
 import BaseClass.CommonUtils;
 
 public class HelloWorld extends CommonUtils {
     public static void main(String[] args) {
         String panther = pantherName();
         System.out.println("Hello "+panther);
+        
     }
+
+    
 }
